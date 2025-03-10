250377923f7b0f1c3067dfa1df436ac778634dbe
bbiscuit0 <dannie.ruiz@student.ateneo.edu>
2025-03-10 22:21:30 +0800

    Added author, created on, and updated on dates for Recipe. Also edited format to follow PEP8

--- ledger/models.py
+++ ledger/models.py
@@ -1,41 +1,46 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     def get_absolute_url(self):
         return reverse('ledger:ingredient_detail', args=[str(self.id)])
-    
+
     def __str__(self):
         return f'{self.name}'
 
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField(max_length=255, default='author')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.id)])
-    
+
     def __str__(self):
         return f'{self.name}'
-    
+
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
 
-    ingredient = models.ForeignKey(Ingredient, 
-        on_delete=models.SET_NULL, 
-        null=True,
-        related_name ='recipe'
-    )
+    ingredient = models.ForeignKey(Ingredient,
+                                   on_delete=models.SET_NULL,
+                                   null=True,
+                                   related_name='recipe'
+                                   )
 
-    recipe = models.ForeignKey(Recipe, 
-        on_delete=models.SET_NULL, 
-        null=True,
-        related_name ='ingredients'
-    )
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               related_name='ingredients'
+                               )
 
     def __str__(self):
         return f'{self.ingredient} : {self.quantity} in {self.recipe}'
